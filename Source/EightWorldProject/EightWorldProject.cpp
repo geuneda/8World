876e80ab2cfeaa8dde0f4e07551cb2481844e9dd
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-05-19 15:55:22 +0900

    init

---
+++ Source/EightWorldProject/EightWorldProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EightWorldProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EightWorldProject, "EightWorldProject" );
+ 
---
+++ Source/EightWorldProject/EightWorldProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EightWorldProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEightWorldProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEightWorldProjectGameMode();
+};
+
+
+
