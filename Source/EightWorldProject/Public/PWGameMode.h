6931a55d874c9250afc94e21a20e8abfc0ef75aa
MinsooJeon <88232069+MinsooJeon@users.noreply.github.com>
2025-05-20 15:58:07 +0900

    20250520 기본 게임맵, 게임모드 C++ BP 추가

---
+++ Source/EightWorldProject/Public/PWGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PWGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIGHTWORLDPROJECT_API APWGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
