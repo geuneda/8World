fd571e30133cce6656835151912201d04f755ed4
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-05-21 17:44:50 +0900

    feat: 나무 및 바위 추가
    - 기본 클래스 추가
    - 메시 추가
    - BP 생성

---
+++ Source/EightWorldProject/Resources/Rock.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Rock.generated.h"
+
+UCLASS()
+class EIGHTWORLDPROJECT_API ARock : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// 스태틱 메시
+	class UStaticMeshComponent* Mesh;
+	// 충돌체
+	class UBoxComponent* Box;
+};
