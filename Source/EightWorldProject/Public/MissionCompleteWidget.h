1a538ddc183daf4c239694a5923f03c8e88a66a2
MinsooJeon <88232069+MinsooJeon@users.noreply.github.com>
2025-06-12 18:52:12 +0900

    20250612
    1.미션완료 UI 간단히 제작

---
+++ Source/EightWorldProject/Public/MissionCompleteWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MissionCompleteWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIGHTWORLDPROJECT_API UMissionCompleteWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
